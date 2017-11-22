4879887bb6549e2092d743ecbe675f389b3cfb25
Kat <katatk@users.noreply.github.com>
2017-11-22 17:04:26 +1300

    ruler animation working

--- js/scroll-reveal.js
+++ js/scroll-reveal.js
@@ -1,13 +1,23 @@
 window.sr = ScrollReveal({
-    reset: true
+    reset: true,
+    opacity: 1,
+    reset: false,
+    mobile: false,
+    duration: 800,
 });
+
 
 // Scroll reveal calls
 window.sr = ScrollReveal();
+
+sr.reveal('.ruler', {
+    origin: 'right',
+    distance: '100%',
+    viewFactor: 0.99,
+});
+
 sr.reveal('.pencil', {
-    delay: 200,
-    opacity: 1,
-    duration: 800,
     origin: 'left',
-    distance: '100%'
+    distance: '100%',
+    viewFactor: 1,
 });
