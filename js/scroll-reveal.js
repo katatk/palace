42d2a2427b27d83f70ff16aeeeda8e961b846bbc
Kat <katatk@users.noreply.github.com>
2017-11-22 14:32:36 +1300

    pencil animating! used scrollreveal plugin

---
+++ js/scroll-reveal.js
@@ -0,0 +1,13 @@
+window.sr = ScrollReveal({
+    reset: true
+});
+
+// Scroll reveal calls
+window.sr = ScrollReveal();
+sr.reveal('.pencil', {
+    delay: 200,
+    opacity: 1,
+    duration: 800,
+    origin: 'left',
+    distance: '100%'
+});
